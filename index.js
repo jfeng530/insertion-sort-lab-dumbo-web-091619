6ac40ea00d62bef519fac56de036cc87891525dc
Jacky Feng <jackyfeng530@yahoo.com>
2020-02-19 18:16:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function findMinAndRemove(array){
-
+  let index = 0
+  let min = array[0]
+  let newArr = []
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i]
+      index = i
+    }
+  }
+  array.slice(index, 1)
+  return min
 }
 
 function insertionSort(array){
-
+  let min
+  let sorted = []
+  while (array.length !== 0) {
+    sorted.push(findMinAndRemove(array))
+  }
+  return sorted
 }
